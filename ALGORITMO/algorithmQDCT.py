9ff7a7237fccb605d05f9b00571d9bafede0d7a8
Pablo Blanco Peris <pabblanc@ucm.es>
2017-05-23 20:32:13 +0200

    .

--- ALGORITMO/algorithmQDCT.py
+++ ALGORITMO/algorithmQDCT.py
@@ -1,23 +1,21 @@
 import cv2
+import math
 import numpy as np
+import quaternion
 from PIL import Image
 import scipy
 from scipy import fftpack
 from scipy.fftpack import fft, dct
+import transformation as TF
 
-#opencv PIL buscar QDCT
 
 #blocksize
 B = 8
-xBegin = 0
-yBegin = 0
-xEnd = 8
-yEnd = 8
-
+#block
+xBegin, yBegin, xEnd, yEnd = 0, 0, 8, 8
 
 imagen = Image.open('/Users/Pablo/Desktop/12.jpeg')
 img = cv2.imread('/Users/Pablo/Desktop/12.jpeg')
-print img
 imageWidth = imagen.size[0]
 imageHeight = imagen.size[1]
 
@@ -32,49 +30,28 @@ while yEnd < imageHeight:
             r = cv2.split(bloque)[2]
             xBegin += 8
             xEnd += 8
-            print bloque
-            qdct_transform = scipy.fftpack.dct(bloque, type=2, n=None, axis=-1, norm=None, overwrite_x=False)
+
+            pixelR = r[0, 0]
+            pixelG = g[0, 0]
+            pixelB = b[0, 0]
+
+            quat = quaternion.quaternion(0,pixelR,pixelG,pixelB)
+            quat2 = np.array([0,pixelR,pixelG,pixelB])
+            test = scipy.fftpack.dct(quat2, type=2, n=None, axis=-1, norm=None, overwrite_x=False)
+            bMatrix = TF.rotation_matrix(0.0, [r[0][0], g[0][0], b[0][0]])
+            quat = TF.quaternion_from_matrix([r[0][0], g[0][0], b[0][0]])
+
+            quaternionOfR = quaternion.as_quat_array(r)
+            quaternionOfG = quaternion.as_quat_array(g)
+            quaternionOfB = quaternion.as_quat_array(b)
+            arrayOfColors = np.concatenate((quaternionOfR,quaternionOfG,quaternionOfB), axis=0)
+            print quaternionOfB
+            qdct_transformR = scipy.fftpack.dct(arrayOfColors, type=2, n=None, axis=-1, norm=None, overwrite_x=False)
+           # quaternionOfColors = quaternion.as_quat_array(arrayOfColors)
+            #qdct_transform = scipy.fftpack.dct(quaternionOfColors, type=2, n=None, axis=-1, norm=None, overwrite_x=False)
         xBegin = 0
         xEnd = 8
         yEnd += 8
         yBegin += 8
 
-#bloque = img[xBegin:xEnd, yBegin:yEnd]
-#bloque = img[np.ix_([0, 7], [0, 7])]
-#print bloque
 
-
-
-
-r,g,b = cv2.split(img)
-
-img8x8 = np.array(img).reshape(8,8)
-
-print r
-
-
-#recibe lista de imagenes o solo una imagen?
-def algorithm(img):
-
-    #move block
-    for r in range(0, img.shape[0] - B, B):
-        for c in range(0, img.shape[1] - B, B):
-            window = img[r:r + B, c:c + B]
-            color = createColorComponents(window)
-          #  makeQdctTransform(color)
-
-
-#return RGB of block
-def createColorComponents(filename, idImage):
-    img1 = cv2.imread(filename, cv2.CV_LOAD_IMAGE_UNCHANGED)
-    h, w = np.array(img1.shape[:2]) / B * B
-    img1 = img1[:h, :w]
-    # Convert BGR to RGB
-    img2 = np.zeros(img1.shape, np.uint8)
-    img2[:, :, 0] = img1[:, :, 2] #R
-    img2[:, :, 1] = img1[:, :, 1] #G
-    img2[:, :, 2] = img1[:, :, 0] #B
-    return img2
-
-
-#def makeQdctTransform():
