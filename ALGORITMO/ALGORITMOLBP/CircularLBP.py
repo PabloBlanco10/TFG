237e756bfd034517ce7f194970f69e2e23a11ff9
Pablo Blanco Peris <pabblanc@ucm.es>
2017-06-02 11:33:59 +0200

    algorithm almost done

--- ALGORITMO/ALGORITMOLBP/CircularLBP.py
+++ ALGORITMO/ALGORITMOLBP/CircularLBP.py
@@ -1,17 +1,8 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-import math
+from PIL import Image as im
 
-
-def bilinear_interpolation(x, y, img):
-    x1, y1 = int(r), int(c)
-    x2, y2 = math.ceil(r), math.ceil(c)
-
-    r1 = (x2 - x) / (x2 - x1) * get_pixel_else_0(img, x1, y1) + (x - x1) / (x2 - x1) * get_pixel_else_0(img, x2, y1)
-    r2 = (x2 - x) / (x2 - x1) * get_pixel_else_0(img, x1, y2) + (x - x1) / (x2 - x1) * get_pixel_else_0(img, x2, y2)
-
-    return (y2 - y) / (y2 - y1) * r1 + (y - y1) / (y2 - y1) * r2
 
 
 def thresholded(center, pixels):
@@ -23,73 +14,82 @@ def thresholded(center, pixels):
             out.append(0)
     return out
 
+def get_pixel_else_0(l, idx, idy, default=0):
+    try:
+        return l[idx,idy]
+    except IndexError:
+        return default
 
-def get_pixel_else_0(l, idx, idy):
-    if idx < int(len(l)) - 1 and idy < len(l[0]):
-        return l[idx, idy]
-    else:
-        return 0
+def make_lbp_with_block():
+    for x in range(0, len(imgCb)):
+        for y in range(0, len(imgCb)):
+            center = imgCb[x, y]
+            top_left = get_pixel_else_0(imgCb, x - 1, y - 1)
+            top_up = get_pixel_else_0(imgCb, x, y - 1)
+            top_right = get_pixel_else_0(imgCb, x + 1, y - 1)
+            right = get_pixel_else_0(imgCb, x + 1, y)
+            left = get_pixel_else_0(imgCb, x - 1, y)
+            bottom_left = get_pixel_else_0(imgCb, x - 1, y + 1)
+            bottom_right = get_pixel_else_0(imgCb, x + 1, y + 1)
+            bottom_down = get_pixel_else_0(imgCb, x, y + 1)
+
+            values = thresholded(center, [top_left, top_up, top_right, right, bottom_right,
+                                          bottom_down, bottom_left, left])
+
+            weights = [1, 2, 4, 8, 16, 32, 64, 128]
+            res = 0
+            for a in range(0, len(values)):
+                res += weights[a] * values[a]
+
+            transformed_img.itemset((x, y), res)
+            # transformed_imgCb.itemset((x,y), res)
+
+        print x
 
 
-img = cv2.imread('/Users/Pablo/Desktop/13.jpeg', 0)
-transformed_img = cv2.imread('/Users/Pablo/Desktop/13.jpeg', 0)
+path = '/Users/Pablo/Desktop/14.jpg'
 
-P = 8  # number of pixels
-R = 1  # radius
+# output of ycbcr.getbands() put in order
+Y = 0
+Cb = 1
+Cr = 2
 
-for x in range(0, len(img)):
-    for y in range(0, len(img[0])):
-        center = img[x, y]
-        pixels = []
-        for point in range(0, P):
-            r = x + R * math.cos(2 * math.pi * point / P)
-            c = y - R * math.sin(2 * math.pi * point / P)
-            if r < 0 or c < 0:
-                pixels.append(0)
-                continue
-            if int(r) == r:
-                if int(c) != c:
-                    c1 = int(c)
-                    c2 = math.ceil(c)
-                    w1 = (c2 - c) / (c2 - c1)
-                    w2 = (c - c1) / (c2 - c1)
+img = cv2.imread(path)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2YCR_CB)
+imgCb = (img[:,:,Cb])
+imgCr = (img[:,:,Cr])
 
-                    pixels.append(int((w1 * get_pixel_else_0(img, int(r), int(c)) + \
-                                       w2 * get_pixel_else_0(img, int(r), math.ceil(c))) / (w1 + w2)))
-                else:
-                    pixels.append(get_pixel_else_0(img, int(r), int(c)))
-            elif int(c) == c:
-                r1 = int(r)
-                r2 = math.ceil(r)
-                w1 = (r2 - r) / (r2 - r1)
-                w2 = (r - r1) / (r2 - r1)
-                pixels.append((w1 * get_pixel_else_0(img, int(r), int(c)) + \
-                               w2 * get_pixel_else_0(img, math.ceil(r), int(c))) / (w1 + w2))
-            else:
-                pixels.append(bilinear_interpolation(r, c, img))
+transformed_imgCb = (imYCbCr[:,:,Cb], "L")[0]
+transformed_imgCr = (imYCbCr[:,:,Cr], "L")[0]
 
-        values = thresholded(center, pixels)
-        res = 0
-        for a in range(0, len(values)):
-            res += values[a] * (2 ** a)
+make_lbp_with_block(transformed_imgCb)
+make_lbp_with_block(transformed_imgCr)
 
-        transformed_img.itemset((x, y), res)
+#return transformed_imgCb, transformed_imgCr
 
-    print x
+im.fromarray(transformed_imgCb).show
+im.fromarray(transformed_imgCr).show
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#im.fromarray(transformed_img).show()
+#return im.fromarray(transformed_imgCb), im.fromarray(transformed_imgCr)
 
-cv2.imshow('image', img)
+'''
 cv2.imshow('thresholded image', transformed_img)
+cv2.waitKey(0)
 
-hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+
+hist,bins = np.histogram(img.flatten(),256,[0,256])
 
 cdf = hist.cumsum()
-cdf_normalized = cdf * hist.max() / cdf.max()
+cdf_normalized = cdf * hist.max()/ cdf.max()
 
-plt.plot(cdf_normalized, color='b')
-plt.hist(transformed_img.flatten(), 256, [0, 256], color='r')
-plt.xlim([0, 256])
-plt.legend(('cdf', 'histogram'), loc='upper left')
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(transformed_img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
 plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+    '''
